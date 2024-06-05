32255e6c47a8f21646f888807ece8fc10246826c
brmcdoug <brmcdoug@cisco.com>
2024-06-05 14:21:20 -0700

    bridges

---
+++ add-bridges.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+
+sudo brctl addbr t00-host1
+sudo brctl addbr t00-host2
+sudo brctl addbr t07-host1
+sudo brctl addbr t02-host2
+
+sudo ip link set up t00-host1
+sudo ip link set up t00-host2
+sudo ip link set up t07-host1
+sudo ip link set up t07-host2
