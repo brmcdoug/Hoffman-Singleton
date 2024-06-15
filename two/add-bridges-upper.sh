7a261f3527e214578234ff0d73792c2f3f5940c5
brmcdoug <brmcdoug@cisco.com>
2024-06-15 16:05:40 -0700

    upper

---
+++ two/add-bridges-upper.sh
@@ -0,0 +1,52 @@
+#/bin/sh
+
+sudo brctl addbr r28-host
+sudo brctl addbr r29-host
+sudo brctl addbr r30-host
+sudo brctl addbr r31-host
+sudo brctl addbr r32-host
+sudo brctl addbr r33-host
+sudo brctl addbr r34-host
+sudo brctl addbr r35-host
+sudo brctl addbr r36-host
+sudo brctl addbr r37-host
+sudo brctl addbr r38-host
+sudo brctl addbr r39-host
+sudo brctl addbr r40-host
+sudo brctl addbr r41-host
+sudo brctl addbr r42-host
+sudo brctl addbr r43-host
+sudo brctl addbr r44-host
+sudo brctl addbr r45-host
+sudo brctl addbr r46-host
+sudo brctl addbr r47-host
+sudo brctl addbr r48-host
+sudo brctl addbr r49-host
+
+sudo brctl addbr r50-host
+sudo brctl addbr r51-host
+
+sudo ip link set up r28-host
+sudo ip link set up r29-host
+sudo ip link set up r30-host
+sudo ip link set up r31-host
+sudo ip link set up r32-host
+sudo ip link set up r33-host
+sudo ip link set up r34-host
+sudo ip link set up r35-host
+sudo ip link set up r36-host
+sudo ip link set up r37-host
+sudo ip link set up r38-host
+sudo ip link set up r39-host
+sudo ip link set up r20-host
+sudo ip link set up r41-host
+sudo ip link set up r42-host
+sudo ip link set up r43-host
+sudo ip link set up r44-host
+sudo ip link set up r45-host
+sudo ip link set up r46-host
+sudo ip link set up r47-host
+sudo ip link set up r48-host
+sudo ip link set up r49-host
+sudo ip link set up r50-host
+sudo ip link set up r51-host
